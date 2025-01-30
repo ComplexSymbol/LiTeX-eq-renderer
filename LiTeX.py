7000e1bc6227f930a729305d0d5b19d5379e42f5
ComplexSymbol <141301057+ComplexSymbol@users.noreply.github.com>
2025-01-30 07:30:14 -0800

    Updated parser, add exponent support (unnested)

--- LiTeX.py
+++ LiTeX.py
@@ -1,144 +1,128 @@
 def genRender(eq):
-    i = 0
-    mode = ""
-    render = []
+  i = 0
+  mode = ""
+  render = [[]]
 
-    while i < len(eq):
-        if eq[i].isdigit() or eq[i] in ("+", "-", "*", "/"):
-            render.append(readGlyph(eq[i]))
+  while i < len(eq):
+    print(f"\nParsing char: \'{eq[i]}\' at index {i} of {eq}")
+    
+    if eq[i].isdigit() or eq[i] in ("+", "-", "*", "/"):
+      print(f"  Appending digit \'{eq[i]}\'")
+      render = add2dArrays(render, readGlyph(eq[i]))
 
-        elif eq[i] == "(":
-            for j in range(1, len(eq[i:]) + 1):
-                if eq[i:][:j].count("(") == eq[i:][:j].count(")"):
-                    render.append(readGlyph("("))
+    elif eq[i] == "(":
+      print(f"  Found parenthesis")
+      for j in range(1, len(eq[i:]) + 1):
+        if eq[i:][:j].count("(") == eq[i:][:j].count(")"):
+          print(f"    Found end of parenthesis at index {i + j} [contents: {eq[i:][:j]}]")
+          print(f"    Recursing contents {eq[i:][:j][1:][:-1]}")
+          
+          render = add2dArrays(render, readGlyph("("))
+          render = add2dArrays(render, genRender(eq[i:][:j][1:][:-1]))
+          render = add2dArrays(render, readGlyph(")"))
+          
+          print(f"    Setting index i to {i + j - 1}")
+          i += j - 1
+          break
 
-                    for r in genRender(eq[i:][:j][1:][:-1]):
-                        render.append(r)
-                    eq = eq.replace(eq[i:][:j], "", 1)
+        if j == len(eq[i:]):
+          raise Exception("Unfinished parenthesis")
 
-                    render.append(readGlyph(")"))
-                    i -= 1
-                    break
+    elif eq[i] == "^":
+      i += 1
+      print(f"Found power at index {i}")
+      
+      if eq[i] == '{':
+        
 
-                if j == len(eq[i:]):
-                    raise Exception("Unfinished  parenthesis")
-
-        elif eq[i] == "^":
-            if eq[i + 1] == '{':
-                i += 1
-                for j in range(1, len(eq[i:]) + 1):
-                    if eq[i:][:j].count("{") == eq[i:][:j].count("}"):
-                        for r in genRender(eq[i:][:j][1:][:-1]):
-                            render.append(r)
-                        eq = eq.replace(eq[i:][:j], "", 1)
-
-                        i -= 1
-                        break
-
-                if j == len(eq[i:]):
-                    raise Exception("Unfinished  brace")
-
-            elif eq[i].isdigit():
-
-
-        i += 1
-
-    return render
+      elif eq[i].isdigit():
+        for j in range(0, len(eq[i:])):
+          if (eq[i:][j].isalnum()):
+            print(f"  Appending power: {eq[i:][j]} at index {i + j}")
+            render = add2dArrays(render, readGlyph("^" + eq[i:][j]), 3, len(readGlyph(eq[i - 2])))
+          else:
+            j -= 1
+            break
+        i += j
+          
+    i += 1
+  
+  print(f"Finished parsing {eq}")
+  return render
 
 
 def readGlyph(g):
-    with open("glyphs.txt", "r") as glyphs:
-        glyphs = glyphs.readlines()
+  with open("glyphs.txt", "r") as glyphs:
+    glyphs = glyphs.readlines()
 
-        for i in range(len(glyphs)):
-            line = glyphs[i].replace("\n", "")
+    for i in range(len(glyphs)):
+      line = glyphs[i].replace("\n", "")
 
-            if line.endswith(":") and line.startswith(g):
-                line = line.replace(g, "").replace(":", "")
+      if line.endswith(":") and line.startswith(g):
+        line = line.replace(g, "").replace(":", "")
 
-                width = 6 if len(line) == 0 else int(line.rpartition("x")[0])
-                height = 10 if len(line) == 0 else int(line.rpartition("x")[2])
+        width = 6 if len(line) == 0 else int(line.rpartition("x")[0])
+        height = 10 if len(line) == 0 else int(line.rpartition("x")[2])
 
-                glyph = [[0] * width for _ in range(height)]
+        glyph = [[0] * width for _ in range(height)]
 
-                for y in range(height):
-                    i += 1
+        for y in range(height):
+          i += 1
 
-                    line = [
-                        1 if digit == "1" else 0 for digit in bin(int(glyphs[i]))[2:]
-                    ]
-                    line = [0] * (width - len(line)) + line
+          line = [
+            1 if digit == "1" else 0 for digit in bin(int(glyphs[i]))[2:]
+          ]
+          line = [0] * (width - len(line)) + line
 
-                    for x in range(width):
-                        glyph[y][x] = line[x]
+          for x in range(width):
+            glyph[y][x] = line[x]
+        
+        return glyph
 
-                return glyph
-
-    raise Exception(f"Glyph  not  found  '{g}'")
+  raise Exception(f"Glyph not found '{g}'")
 
 
-def add2dArrays(a, b, overlap = None, resizeAParenTo = None, resizeBParenTo = None, relativeHA = None):
-    if relativeHA == None:
-        relativeHA = len(a)
-
-    if resizeAParenTo != None:
-        for _ in range(resizeAParenTo - len(a)):
-            a.insert(3, [0, 1, 0, 0])
-
-    if resizeBParenTo != None:
-        for _ in range(resizeAParenTo - len(b)):
-            a.insert(3, [0, 0, 0, 1])
-
-    newArray = [
-        [0] * len(a[0] + b[0])
-        for _ in range(
-            max(len(b), len(a)) if overlap == None else (len(a) + len(b) - overlap)
-        )
+def add2dArrays(a, b, overlap = -1, relHt = -1):
+  if relHt == -1:
+    relHt = len(a)
+  
+  newArray = [
+    [0] * (len(a[0]) + len(b[0]))
+      for _ in range(
+        max(len(b), len(a)) if overlap == -1 else (relHt + len(b) - overlap)
+      )
     ]
+  newArray = merge2dArrays(newArray, a, 0, 0)
+  newArray = merge2dArrays(
+    newArray, b, len(a[0]), 0 if overlap == -1 else (relHt - overlap)
+  )
 
-    newArray = mergeBitmaps(newArray, a, 0, 0)
-
-    newArray = mergeBitmaps(
-        newArray, b, len(a[0]), 0 if overlap == None else (relativeHA - overlap)
-    )
-
-    return newArray
-
-
+  return newArray
 
 def merge2dArrays(a, b, Mx, My):
-    My = len(a) - My
-    for y in range(len(a)):
-        toMerge = (
-            [0] * len(a[0])
-            if not (y <= My and y > My - len(b))
-            else ([0] * Mx) + b[y - My] + ([0] * (len(a[0]) - len(b[0]) - Mx))
-        )
-
+  My = len(a) - My
+  for y in range(len(a)):
+    toMerge = (
+      [0] * len(a[0])
+      if not (y <= My and y > My - len(b))
+      else ([0] * Mx) + b[y - My] + ([0] * (len(a[0]) - len(b[0]) - Mx))
+    )
     a[y] = [max(a[y][i], toMerge[i]) for i in range(len(toMerge))]
 
-    return a
+  return a
 
 
 def print2dArray(arr):
-    for y in range(len(arr)):
-        for x in range(len(arr[0])):
-            print(arr[y][x], end=" ")
-        print()
+  for y in range(len(arr)):
+    print(f"{' ' * (len(str(len(arr))) - len(str(y)))}{y}: {arr[y]}", end=",\n")
 
-    for y in range(len(arr)):
-        print(f"{y}: {arr[y]}", end=",\n")
-
-    print("--PRERENDER-- **NOT TO SCALE**")
-    for y in range(len(arr)):
-        for x in range(len(arr[0])):
-            print("█" if arr[y][x] == 1 else " ", end="")
-        print()
-    print("--PRERENDER--")
+  print("--PRERENDER-- **NOT TO SCALE**")
+  for y in range(len(arr)):
+    for x in range(len(arr[0])):
+      print("█" if arr[y][x] == 1 else " ", end="")
+    print()
+  print("--PRERENDER--")
+  
+print2dArray(genRender("1+(2^3-4)^5+6^{7^7}"))
 
 
-rendered = [[]]
-for k in genRender("1+2-(3-(4-5)^2-6)"):
-    rendered = add2dArrays(rendered, k)
-
-print2dArray(rendered)
