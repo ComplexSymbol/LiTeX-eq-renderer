a72d67a799164ec4eddcc5a75e282cae53f91040
ComplexSymbol <141301057+ComplexSymbol@users.noreply.github.com>
2025-04-12 18:30:01 -0700

    Rewrote operator code
    it was mad trash
    made it more robust by switching to incremental finder

---
+++ Evaluator.py
@@ -0,0 +1,315 @@
+import math
+import cmath
+
+
+trigs = {
+  'sin': lambda x: cmath.sin(x) if isinstance(x, complex) else math.sin(x),
+  'cos': lambda x: cmath.cos(x) if isinstance(x, complex) else math.cos(x),
+  'tan': lambda x: cmath.tan(x) if isinstance(x, complex) else math.tan(x),
+  'csc': lambda x: 1 / (cmath.sin(x) if isinstance(x, complex) else math.sin(x)),
+  'sec': lambda x: 1 / (cmath.cos(x) if isinstance(x, complex) else math.cos(x)),
+  'cot': lambda x: 1 / (cmath.tan(x) if isinstance(x, complex) else math.tan(x)),
+  'asin': lambda x: cmath.asin(x) if isinstance(x, complex) else math.asin(x),
+  'acos': lambda x: cmath.acos(x) if isinstance(x, complex) else math.acos(x),
+  'atan': lambda x: cmath.atan(x) if isinstance(x, complex) else math.atan(x)
+}
+
+operate = {
+  0: lambda x, y: toFloat(x) / toFloat(y),
+  1: lambda x, y: toFloat(x) * toFloat(y),
+  2: lambda x, y: toFloat(x) + toFloat(y),
+  3: lambda x, y: toFloat(x) - toFloat(y)
+}
+
+# Follows strict PEDMAS
+def Evaluate(eq, solve = False, replace = False, guess = 10):
+  global trigs, operate
+  
+  if isFloat(eq):
+    return toFloat(eq)
+  
+  print(f"Standardizing eq: '{eq}'")
+  
+  if replace:
+    if '`' in eq:
+      raise ValueError("Incomplete expression")
+
+    #eq = eq.replace(" - ", " +-")
+    eq = eq.replace("~", "-")
+    eq = eq.replace("log-", "log_")
+    eq = eq.replace(r"\im", "j")
+  
+  eq = impMult(eq)
+  
+  # First, change constants into numbers
+  specials = ["pi", "e"]
+  values = { 'e': math.e, 
+             'pi': math.pi,
+           }
+  tries = [("\\" + sp) in eq for sp in specials]
+  while any(tries):
+    eq = eq.replace("\\" + specials[tries.index(True)], 
+                    str(values[specials[tries.index(True)]]))
+    tries[tries.index(True)] = False
+  eq = impMult(eq)
+  #print(f"   Eq is {eq} after replacing constants")
+  
+  eq = eq.replace(r"*\perm*", r"\perm").replace(r"*\comb*", r"\comb")
+  
+  if solve:
+    return NewtonMethod(eq, guess, 5, True)
+  
+  #print(f"Evaluating \'{eq}\'")
+  
+  # P - Parenthetical function (trig)
+  while any(trigs in eq for trigs in ["sin", "cos", "tan", "sec", "csc", "cot"]):
+    func = "sin" if "sin" in eq else (
+           "cos" if "cos" in eq else (
+           "tan" if "tan" in eq else (
+           "csc" if "csc" in eq else (
+           "sec" if "sec" in eq else (
+           "cot" if "cot" in eq else (
+           None))))))
+   
+    if (eq.index(func + "^{-1}") if func + "^{-1}" in eq else None) == eq.index(func):
+      func = "a" + func
+    
+    contents = Between(eq[eq.index(func[1:] if func[0] == "a" else func) + (8 if func[0] == "a" else 3):], "(", ")")
+    
+    #print(f"  Found trig function {func} with contents {contents}")
+    eq = eq.replace(f"{func[1:] + "^{-1}" if func[0] == "a" else func}({contents})", 
+                    str(trigs[func](Evaluate(contents))))
+
+  # P - Parenthetical function (log)  
+  while "log" in eq: 
+    location = eq.index("log")
+    base = Between(eq[location + 3:], "{", "}")
+    contents = Between(eq[location + len(base) + 6:], "(", ")")
+    #print(f"  Found logarithm with base {base} and contents {contents}")
+    
+    eq = eq.replace("log_{"+base+"}" + f"({contents})", 
+                    str(cmath.log(Evaluate(contents), Evaluate(base))))
+  
+
+  # P - Parenthesis
+  while "(" in eq:
+    contents = Between(eq, "(", ")")
+    #print(f"  Found parenthesis with contents{contents}")
+    
+    sign = 1
+    if eq[eq.index("(" + contents) - 1] == "-":
+      sign = -1
+      eq = eq[:eq.index("(" + contents) - 1] + eq[eq.index("(" + contents):]
+
+    eq = eq.replace(f"({contents})", 
+                    str(sign * Evaluate(contents)).replace("(", "").replace(")", ""), 1)
+
+  # E - Exponents
+  while "^" in eq:
+    exp = Between(eq[eq.index("^"):], "{", "}")
+    base = None
+    
+    i = 0
+    base = eq[:eq.index("^")]
+    while not isFloat(base): 
+      i += 1
+      base = eq[:eq.index("^")][i:]
+
+    if eq[eq.index(base) - 1] == "+":
+      base = base[1:]
+    
+    #print(f"  Found exponent with base {base} and exponent {exp}")
+    eq = eq.replace(base + "^{"+exp+"}",
+                    str(toFloat(pow(toFloat(base), Evaluate(exp)))))
+
+  # E - Exponents (square root)
+  while r"\sqrt" in eq:
+    start = eq.find(r"\sqrt") + 5
+    nthroot = Between(eq[start:], "{", "}")
+    contents = Between(eq[start + len(nthroot) + 2:], "{", "}")
+    #print(f"  Found a {nthroot} degree radical containing {contents}")
+    
+    eq = eq.replace(r"\sqrt{"+nthroot+"}" + "{"+contents+"}", 
+                    str(toFloat(pow(Evaluate(contents), 1 / Evaluate(nthroot)))))
+
+  # D - Division (Fractions have priority)
+  while r"\frac" in eq:
+    start = eq.find(r"\frac") + 5
+    numer = Between(eq[start:], "{", "}")
+    denom = Between(eq[start + len(numer) + 2:], "{", "}")
+    #print(f"  Found a fraction with numerator {numer} and denominator {denom}")
+    
+    eq = eq.replace(r"\frac{"+numer+"}" + "{"+denom+"}", 
+                    str(toFloat(Evaluate(numer) / Evaluate(denom))))
+
+  # Reevaluate parenthesis because complex numbers
+  while "(" in eq:
+    contents = Between(eq, "(", ")")
+    #print(f"  Found parenthesis with contents{contents}")
+    
+    sign = 1
+    if eq[eq.index("(" + contents) - 1] == "-":
+      sign = -1
+      eq = eq[:eq.index("(" + contents) - 1] + eq[eq.index("(" + contents):]
+
+    eq = eq.replace(f"({contents})", 
+                    str(sign * Evaluate(contents)).replace("(", "").replace(")", ""), 1)
+  
+  # Permutations and Combinations
+  while r"\perm" in eq or r"\comb" in eq:
+    isComb = r"\comb" in eq
+    indx = eq.index(r"\comb" if isComb else r"\perm")
+    
+    i = 0
+    while not isFloat(eq[:indx][i:]):
+      i += 1
+    n = eq[:indx][i:]
+    
+    i = len(eq)
+    while not isFloat(eq[indx + 5:][:i]):
+      i -= 1
+    r = eq[indx + 5:][:i]
+    
+    eq = eq.replace(n + (r"\comb" if isComb else r"\perm") + r,
+                    str(math.perm(int(n), int(r)) / (math.factorial(int(r)) if isComb else 1)))
+  
+  # DMAS - In that order
+  eq = eq.replace(" * ", "*").replace(" + ", "+").replace(" / ", "/")
+  eq = ("0" + eq) if eq[0] == "-" else eq
+  progress = -1
+  curOp = 0
+  print(f"  Finding operations in {eq}")
+  while set(eq) & frozenset("/*+-") and not isFloat(eq):
+    opStr = ("/", "*", "+", "-")[curOp]
+    
+    if not opStr in eq:
+      curOp += 1
+      progress = -1
+      continue
+    
+    progress += 1
+    
+    if eq[progress] != opStr:
+      continue
+    
+    if " " in eq and curOp == 3:
+      eq = eq.replace(" ", "")
+      progress = -1
+      continue
+    
+    print(f"eq, {eq[progress:]}")
+    
+    i = 0
+    while not opIsFloat(eq[:progress][i:]): i += 1
+    first = eq[:progress][i:]
+    
+    i = len(eq[progress + 1:])
+    while not opIsFloat(eq[progress + 1:][:i]): i -= 1
+    second = eq[progress + 1:][:i]
+  
+    repl = str(toFloat(operate[curOp](first, second))).replace("(", "").replace(")", "")
+    print(f"  Found operator \'{eq[progress]}\' with first \'{first}\' and second \'{second}\'. Result: {repl}")
+    eq = eq.replace(f"{first}{eq[progress]}{second}", 
+                    repl)
+    progress = progress + len(repl) - 1
+    
+  ans = toFloat(eq)
+  print(f"  Finished evaluating; result: {ans}")
+  return complex(round(complex(ans).real, 15), round(complex(ans).imag, 15)) if replace else ans
+
+def primeFactors(n):
+  i = 2
+  factors = []
+  while i * i <= n:
+    if n % i:
+      i += 1
+    else:
+      n //= i
+      factors.append(i)
+  if n > 1:
+    factors.append(n)
+  return factors
+  
+# Add implicit multiplication
+def impMult(eq):
+  i = 0
+  while i < len(eq) - 1:
+    # 3 main causes: num*func, paren*func, paren*paren
+    if ((eq[i].isdigit() and eq[i + 1] == "\\") or # 5\pi
+        (eq[i].isdigit() and eq[i + 1] == "(") or # 2(3 ...
+        (eq[i].isdigit() and eq[i + 1].isalpha() and eq[i + 1] != "e" and eq[i + 1] != "j") or # 9sin(... but not 3e
+        (eq[i].isalpha() and eq[i + 1].isdigit()) or # \pi3...
+        (eq[i].isalpha() and eq[i + 1] == "\\") or # \pi\e, X: (\pi
+        (eq[i] == "j" and eq[i + 1].isalpha()) or # jsin(...
+        (eq[i] == ")" and eq[i + 1] == "\\") or # ...4)\pi
+        (eq[i] == ")" and eq[i + 1] == "(") or # ...3)(6...
+        (eq[i] == ")" and eq[i + 1].isdigit()) or # ...)7
+        (eq[i] == "}" and eq[i + 1].isdigit())): # ...2}7
+      eq = eq[:i + 1] + "*" + eq[i + 1:] # Insert multiplication
+    i += 1
+  return eq
+
+# Finds string subset between char1 and char2, with nesting support
+def Between(string, char1, char2):
+  for i in range(len(string)):
+    if string[i] == char2 and (string[:i].count(char1) == string[:i + 1].count(char2)):
+      return string[string.index(char1) + 1 : i]
+  
+  raise ValueError(f"Unable to find contents between {char1} and {char2}")
+
+def isFloat(string):
+  if string[0] == "+" or string[0] == " ": return False
+  
+  try:
+    toFloat(string)
+    return True
+
+  except ValueError:
+    return False
+
+def opIsFloat(string):
+  return isFloat(string) and ((complex(toFloat(string)).real == 0 and 
+                               complex(toFloat(string)).imag != 0) or 
+                              (complex(toFloat(string)).real != 0 and 
+                               complex(toFloat(string)).imag == 0))
+    
+def toFloat(string):
+  string = str(string)
+  
+  # Complex or imaginary
+  if "j" in string:
+    return complex(string)
+  
+  else: return float(string)
+  
+inc = 0.001
+def NewtonMethod(eq, guess, accuracy, shouldGuessImag, epsilon = 0.001, maxIter = 40):
+  if shouldGuessImag:
+    guess = guess + 0.1j
+
+  for i in range(maxIter):
+    if i == maxIter - 1:
+      raise ValueError(f"No root found under {maxIter} iterations")
+    
+    inputEQ = eq.replace("x", str(guess))
+    funcAtGuess = Evaluate(inputEQ)
+    if abs(complex(funcAtGuess).real + complex(funcAtGuess).imag) < 2 * math.pow(10, -accuracy - 1):
+      break
+    
+    derivAtGuess = (funcAtGuess - Evaluate(eq.replace("x", str(guess - inc)))) / inc
+    if abs(derivAtGuess) < epsilon:
+      raise ValueError(f"Function too flat... Possible asymptote. Try another guess. {derivAtGuess}")
+    
+    testGuess = (guess * derivAtGuess - funcAtGuess) / derivAtGuess
+    if not shouldGuessImag and complex(Evaluate(eq.replace("x", str(testGuess)))).imag != 0:
+      print(f"Decrementing guess {guess} by 0.5")
+      guess += -0.5 if derivAtGuess.real < 0 else 0.5
+      continue
+    
+    guess = testGuess
+    print(f"New Guess: {guess}")
+    
+  
+  return complex(round(complex(guess).real, accuracy), round(complex(guess).imag, accuracy))
+  
+    
---
+++ LiTeX.py
@@ -0,0 +1,386 @@
+import Evaluator
+import math
+#import DictGen
+
+lastFinishedBarHt = None
+logging = False
+
+glyphDict = {
+  '(': 0,
+  ')': 11,
+  '0': 22,
+  '1': 33,
+  '2': 44,
+  '3': 55,
+  '4': 66,
+  '5': 77,
+  '6': 88,
+  '7': 99,
+  '8': 110,
+  '9': 121,
+  '+': 132,
+  '*': 143,
+  '-': 154,
+  '~': 165,
+  '/': 176,
+  '.': 187,
+  'x': 198,
+  's': 209,
+  'i': 220,
+  'im': 231,
+  'n': 242,
+  'c': 253,
+  'o': 264,
+  't': 275,
+  'a': 286,
+  'l': 297,
+  'g': 308,
+  'h': 319,
+  'pi': 330,
+  'e': 341,
+  'comb': 352,
+  'perm': 363,
+  '=': 374,
+  'f': 385,
+  '`': 396,
+  'rad': 407,
+  '^(': 413,
+  '^)': 421,
+  '^+': 429,
+  '^-': 437,
+  '^*': 445,
+  '^/': 453,
+  '^0': 461,
+  '^1': 469,
+  '^2': 477,
+  '^3': 485,
+  '^4': 493,
+  '^5': 501,
+  '^6': 509,
+  '^7': 517,
+  '^8': 525,
+  '^9': 533,
+  '^x': 541,
+  '^s': 549,
+  '^im': 557,
+  '^i': 565,
+  '^n': 573,
+  '^c': 581,
+  '^o': 589,
+  '^t': 597,
+  '^a': 605,
+  '^l': 613,
+  '^g': 621,
+  '^h': 629,
+  '^pi': 637,
+  '^e': 645,
+  '^~': 653,
+  '^`': 661,
+  '^.': 669,
+}
+
+def genRender(eq, exp=False):
+  global lastFinishedBarHt
+  i = 0
+  begWth = "" if exp == False else "^"
+  lastHeight = 0
+  barHt = 2 if exp else 4
+  render = [[]] * (7 if exp else 10)
+  hang = 0
+  eq = eq.replace(" ", "")
+  
+  while i < len(eq):
+    if eq[i] == " ":
+      i += 1
+      continue
+    print(f"Parsing char: {eq[:i] + ' [' + eq[i] + '] ' + eq[i + 1:]} with hang {hang}")
+    
+    if eq[i].isdigit() or eq[i].isalpha() or eq[i] in ("+", "-", "*", "/", "=", ".", "~", "`"):
+      #print(f" Appending digit '{eq[i]}'")
+      char = readGlyph(begWth + eq[i])
+      render = add2dArrays(render, char, AbarHt=barHt)
+      lastHeight = len(char)
+      
+      del char
+  
+    elif eq[i] == "(":
+      #print(f" Found parenthesis")
+      contents = Evaluator.Between(eq[i:], "(", ")")
+      renderedConts = genRender(contents, exp)
+      parenHeight = max(0, len(renderedConts) - (7 if exp else 10))
+      
+      
+      render = add2dArrays(
+        render,
+        readGlyph(begWth + "(", -parenHeight, exp),
+        AbarHt=barHt,
+        BbarHt=lastFinishedBarHt,
+      )
+      barHt = max(barHt, lastFinishedBarHt)
+      render = add2dArrays(
+        render,
+        renderedConts,
+        AbarHt=barHt,
+        BbarHt=lastFinishedBarHt,
+      )
+      render = add2dArrays(
+        render,
+        readGlyph(begWth + ")", parenHeight, exp),
+        AbarHt=barHt,
+        BbarHt=lastFinishedBarHt,
+      )
+      lastHeight = parenHeight + (7 if exp else 10) + hang
+      
+      #print(f"  setting i to {len(contents) + 1}")
+      i += len(contents) + 1
+      
+      del contents, renderedConts, parenHeight
+    
+    elif eq[i] == "^" or eq[i] == "_":
+      isPwr = eq[i] == "^"
+      #print(f"Found exponent/subscript at index {i}")
+      
+      contents = Evaluator.Between(eq[i:], "{", "}")
+      ln = len(contents)
+      contents = genRender(contents, True)
+      
+      #print(f"LastHeight: {lastHeight}")
+      if isPwr:
+        render = add2dArrays(
+          render, contents, overlap=3 if exp else 4, relHt=lastHeight
+        )
+      else:
+        render = add2dArrays(render, contents, AbarHt=barHt, BbarHt=len(contents) - (2 if exp else 0), add = len(contents) - barHt)
+        barHt += abs(min(barHt - len(contents), 0))
+      
+      i += ln + 2
+      
+      del isPwr, contents, ln
+    
+    elif eq[i] == "\\":
+      #print("Found escape sequence")
+      esc = None
+      # Operator or escape character?
+      try:
+        esc = Evaluator.Between(eq[i:], "\\", "{")
+      except ValueError: pass
+    
+      # Escape character (Special Character)
+      if esc == None or not esc.isalpha():
+        specials = ["pi", "e", 'im', "perm", "comb"]
+        tries = [eq[i + 1:].startswith(ch) for ch in specials]
+        
+        # None of the accepted special characters were found
+        if not any(tries):
+          raise ValueError(f"Unidentified special character starting at: {eq[i:]}")
+        
+        #print(f"Found special character: {specials[tries.index(True)]}")
+        
+        # Generate render for character, then append it to the render and increment i
+        char = readGlyph(begWth + specials[tries.index(True)])
+        render = add2dArrays(render, char, AbarHt = barHt)
+        lastHeight = len(char)
+        i += len(specials[tries.index(True)])
+        
+        del specials, tries, char
+      
+      # Since it's not an escape character, it must be a function
+      elif esc == "frac":
+        # Numerator & Denominator for fraction.
+        num = Evaluator.Between(eq[i:], "{", "}")
+        den = Evaluator.Between(eq[i + len(num) + 7:], "{", "}")
+        #print(f"Setting i to: {i + len(num) + len(den) + 8}" )
+        i += len(num) + len(den) + 8
+        
+        num = genRender(num, True)
+        den = genRender(den, True)
+
+        fraction = [
+          [False] * (max(len(num[0]), len(den[0])) + 1 + (max(len(num[0]), len(den[0])) % 2))
+          for _ in range(len(num) + len(den) + 2)
+        ]
+        fraction = merge2dArrays(
+          fraction,
+          num,
+          (len(fraction[0]) // 2) - (len(num[0]) // 2),
+          len(den) + 2,
+        )
+        fraction = merge2dArrays(
+          fraction,
+          den,
+          (len(fraction[0]) // 2) - (len(den[0]) // 2),
+          0,
+        )
+        fraction = merge2dArrays(
+          fraction,
+          [[True] * (len(fraction[0]) - 1)],
+          1,
+          len(den) + 1,
+        )
+        render = add2dArrays(
+          render, fraction, AbarHt=barHt, BbarHt=len(den)
+        )
+        barHt = max(barHt, len(den)) + hang
+        lastHeight = len(fraction) + hang
+        
+        del num, den, fraction
+        
+      elif esc == "sqrt":
+        # Nth root
+        n = Evaluator.Between(eq[i:], "{", "}")
+        radicand = Evaluator.Between(eq[i + len(n) + 7:], "{", "}")
+        i += len(n) + len(radicand) + 8
+        
+        n = genRender(n, True) if n != "2" else [[False, False]]
+        radicand = genRender(radicand, exp)
+        
+        stem = [[False, False]] + ([[False, True]] * ((len(radicand) + 1) // 2))
+        stem += [[True, False]] * ((len(radicand) - (len(radicand) + 1) // 2) - 4)
+        radical = [[False] * (5 + len(radicand[0]) + len(n[0])) for _ in range(max(len(radicand) + 2, len(radicand) + len(n) - 2))]
+        radical = merge2dArrays(radical, n, 0, (len(radical) - len(n)) // 2)
+        radical = merge2dArrays(radical, readGlyph("rad"), len(n[0]) - 2, 0)
+        radical = merge2dArrays(radical, stem, len(n[0]) + 1, 4)
+        radical = merge2dArrays(radical, radicand, len(n[0]) + 3, 0)
+        radical = merge2dArrays(radical, [[True] * (len(radicand[0]) + 3)], len(n[0]) + 2, len(radicand) + 1)
+        radical = merge2dArrays(radical, [[True], [True]], len(radicand[0]) + len(n[0]) + 4, len(radicand) - 1)
+        
+        render = add2dArrays(render, radical, AbarHt = barHt, BbarHt = lastFinishedBarHt)
+        barHt = max(barHt, lastFinishedBarHt) + hang
+        lastHeight = len(radical) + hang
+        
+        del n, radicand, stem, radical
+      
+      del esc
+
+    else:
+      raise Exception(f" Unidentified character: {eq[i]}")
+    
+    i += 1
+
+  #print("Removing overhead...")
+  count = 0
+  while True:
+    if all(not p for p in render[0]):
+      del render[0]
+      count += 1
+    else:
+      break
+  render.insert(0, [False] * len(render[0]))
+  print(f"Removed {max(0,count - 1)} empty overhead lines")
+  
+  print(f"Finished parsing {eq}")
+  lastFinishedBarHt = barHt
+  return render
+
+
+def readGlyph(g, resParen = 0, exp = False):
+  with open("glyphs.txt", "r") as glyphs:
+    glyphs = glyphs.readlines()
+    line = glyphs[glyphDict[g]].replace("\n", "").replace(g, "", 1).replace(":", "")
+
+    width = 6 if len(line) == 0 else int(line.rpartition("x")[0])
+    height = 10 if len(line) == 0 else int(line.rpartition("x")[2])
+    
+    glyph = [[False] * width] * height
+
+    for y in range(height):
+      line = [
+        True if digit == "1" else False
+        for digit in bin(int(glyphs[glyphDict[g] + y + 1]))[2:]
+      ]
+      line = [False] * (width - len(line)) + line
+      glyph[y] = line
+      
+    for p in range(abs(resParen)):
+      glyph.insert(
+        4,
+        ([False, True, False] if resParen < 0 else [False, False, True]) if exp 
+        else ([False, True, False, False]
+          if resParen < 0
+          else [False, False, False, True]
+          )
+      )
+
+    return glyph
+
+  raise Exception(f"Glyph not found '{g}'")
+
+
+def add2dArrays(a, b, overlap=-1, relHt=-1, AbarHt=-1, BbarHt=-1, add = 0):
+  if relHt == -1:
+    relHt = len(a)
+  if AbarHt == -1:
+    AbarHt = (len(a) // 2) - 1
+  if BbarHt == -1:
+    BbarHt = (len(b) // 2) - 1
+  diff = (AbarHt - BbarHt) if overlap == -1 else 0
+
+  #print(f"Add2dArrays ARGS: overlap: {overlap}, relHt: {relHt}, AbarHt: {AbarHt}, BbarHt: {BbarHt}, diff: {diff}")
+
+  newArray = [[False] * (len(a[0]) + len(b[0]))] * (
+    max(
+      max(len(a), len(b)) + ((len(b) - BbarHt - 1) - (len(a) - AbarHt - 1)),
+      len(a),
+      len(b),
+    ) + add
+    if overlap == -1
+    else max(len(a), relHt + len(b) - overlap)
+  )
+  newArray = merge2dArrays(newArray, a, 0, abs(diff) if diff < 0 else 0)
+  newArray = merge2dArrays(
+    newArray,
+    b,
+    len(a[0]),
+    (diff if diff > 0 else 0) if overlap == -1 else (relHt - overlap),
+  )
+
+  return newArray
+
+
+def merge2dArrays(a, b, x, y):
+  y = len(a) - y
+  for h in range(len(a)):
+    toMerge = (
+      [False] * len(a[0])
+      if not (h <= y and h > y - len(b))
+      else ([False] * x) + b[h - y] + ([False] * (len(a[0]) - len(b[0]) - x))
+    )
+    a[h] = [max(a[h][i], toMerge[i]) for i in range(len(toMerge))]
+
+  return a
+
+
+def print2dArray(arr, bh = None):
+  if bh != None:
+    H = [[False, False]] * len(arr)
+    H[len(H) - 1 - bh] = [True, False]
+    arr = add2dArrays(H, arr)
+  
+  print(f"Dimensions: {len(arr[0])}x{len(arr)}")
+  print("--PRERENDER--")
+  for y in range(len(arr)):
+    for x in range(len(arr[0])):
+      print("██" if arr[y][x] else "  ", end="")
+    print()
+  print("--PRERENDER--")
+  
+equation = r"4 - 3 - 4\im"
+eq = equation
+
+if "=" in eq:
+  indx = eq.index("=")
+  eq = eq[:indx] + " - ("+eq[indx + 1:]+")"
+  
+ans = str(Evaluator.Evaluate(eq, "x" in eq, True, 0.2))
+ans = (float(ans) if isinstance(ans, float) else
+      "{0:g}".format(complex(ans).real) + ("{0:+g}".format(complex(ans).imag) + r"\im" if 
+                                complex(ans).imag != 0 else "")).replace("j", r"\im")
+ans = "~" + ans[1:] if ans[0] == "-" else ans
+
+renderEQ = genRender(equation)
+renderANS = genRender(("x" if "x" in equation else "") + "=" + ans)
+
+print2dArray(renderEQ)
+print2dArray(renderANS)
+
+
