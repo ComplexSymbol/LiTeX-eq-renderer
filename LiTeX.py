0a8a277164343f5ce5fb8e4067df7c8fa126b2d5
ComplexSymbol <141301057+ComplexSymbol@users.noreply.github.com>
2025-04-23 22:14:06 -0700

    Major optimizations in LT, EV, and SPI
    it's even faster now!!!!

--- LiTeX.py
+++ LiTeX.py
@@ -1,386 +1,298 @@
 import Evaluator
 import math
-#import DictGen
+import prerendered
 
 lastFinishedBarHt = None
-logging = False
-
-glyphDict = {
-  '(': 0,
-  ')': 11,
-  '0': 22,
-  '1': 33,
-  '2': 44,
-  '3': 55,
-  '4': 66,
-  '5': 77,
-  '6': 88,
-  '7': 99,
-  '8': 110,
-  '9': 121,
-  '+': 132,
-  '*': 143,
-  '-': 154,
-  '~': 165,
-  '/': 176,
-  '.': 187,
-  'x': 198,
-  's': 209,
-  'i': 220,
-  'im': 231,
-  'n': 242,
-  'c': 253,
-  'o': 264,
-  't': 275,
-  'a': 286,
-  'l': 297,
-  'g': 308,
-  'h': 319,
-  'pi': 330,
-  'e': 341,
-  'comb': 352,
-  'perm': 363,
-  '=': 374,
-  'f': 385,
-  '`': 396,
-  'rad': 407,
-  '^(': 413,
-  '^)': 421,
-  '^+': 429,
-  '^-': 437,
-  '^*': 445,
-  '^/': 453,
-  '^0': 461,
-  '^1': 469,
-  '^2': 477,
-  '^3': 485,
-  '^4': 493,
-  '^5': 501,
-  '^6': 509,
-  '^7': 517,
-  '^8': 525,
-  '^9': 533,
-  '^x': 541,
-  '^s': 549,
-  '^im': 557,
-  '^i': 565,
-  '^n': 573,
-  '^c': 581,
-  '^o': 589,
-  '^t': 597,
-  '^a': 605,
-  '^l': 613,
-  '^g': 621,
-  '^h': 629,
-  '^pi': 637,
-  '^e': 645,
-  '^~': 653,
-  '^`': 661,
-  '^.': 669,
-}
-
-def genRender(eq, exp=False):
+def genRender(eq, exp=False, first=False):
   global lastFinishedBarHt
   i = 0
   begWth = "" if exp == False else "^"
   lastHeight = 0
   barHt = 2 if exp else 4
-  render = [[]] * (7 if exp else 10)
+  render = Render(0, [])
   hang = 0
   eq = eq.replace(" ", "")
-  
+
   while i < len(eq):
-    if eq[i] == " ":
-      i += 1
-      continue
-    print(f"Parsing char: {eq[:i] + ' [' + eq[i] + '] ' + eq[i + 1:]} with hang {hang}")
-    
+    #print(f"Parsing char: {eq[:i] + ' [' + eq[i] + '] ' + eq[i + 1:]} with hang {hang}")
+
+    # Trivial characters
     if eq[i].isdigit() or eq[i].isalpha() or eq[i] in ("+", "-", "*", "/", "=", ".", "~", "`"):
       #print(f" Appending digit '{eq[i]}'")
       char = readGlyph(begWth + eq[i])
-      render = add2dArrays(render, char, AbarHt=barHt)
-      lastHeight = len(char)
-      
+      render = addRenders(render, char, AbarHt=barHt)
+      lastHeight = len(char.bitmap)
+
       del char
-  
-    elif eq[i] == "(":
+
+    elif eq[i] == "(" or eq[i] == "[":
       #print(f" Found parenthesis")
-      contents = Evaluator.Between(eq[i:], "(", ")")
+      pair = ("(", ")") if eq[i] == "(" else ("[", "]")
+      contents = Evaluator.Between(eq, i, pair[0], pair[1])
       renderedConts = genRender(contents, exp)
-      parenHeight = max(0, len(renderedConts) - (7 if exp else 10))
-      
-      
-      render = add2dArrays(
+      parenHeight = max(0, len(renderedConts.bitmap) - (7 if exp else 10))
+
+      # Opening parenthesis
+      render = addRenders(
         render,
-        readGlyph(begWth + "(", -parenHeight, exp),
+        readGlyph(begWth + pair[0], -parenHeight, exp, pair[0] == "["),
         AbarHt=barHt,
         BbarHt=lastFinishedBarHt,
       )
+      
       barHt = max(barHt, lastFinishedBarHt)
-      render = add2dArrays(
+      # Contents
+      render = addRenders(
         render,
         renderedConts,
         AbarHt=barHt,
         BbarHt=lastFinishedBarHt,
       )
-      render = add2dArrays(
+      # Closing parenthesis
+      render = addRenders(
         render,
-        readGlyph(begWth + ")", parenHeight, exp),
+        readGlyph(begWth + pair[1], parenHeight, exp, pair[0] == "["),
         AbarHt=barHt,
         BbarHt=lastFinishedBarHt,
       )
       lastHeight = parenHeight + (7 if exp else 10) + hang
-      
+
       #print(f"  setting i to {len(contents) + 1}")
       i += len(contents) + 1
-      
-      del contents, renderedConts, parenHeight
-    
+      del contents, renderedConts, parenHeight, pair
+
     elif eq[i] == "^" or eq[i] == "_":
       isPwr = eq[i] == "^"
       #print(f"Found exponent/subscript at index {i}")
-      
-      contents = Evaluator.Between(eq[i:], "{", "}")
-      ln = len(contents)
+
+      contents = Evaluator.Between(eq, i, "{", "}")
+      i += len(contents) + 2
       contents = genRender(contents, True)
-      
-      #print(f"LastHeight: {lastHeight}")
+
       if isPwr:
-        render = add2dArrays(
-          render, contents, overlap=3 if exp else 4, relHt=lastHeight
+        render = addRenders(
+          render, contents, overlap=3 if exp else 4, relHt=lastHeight + hang
         )
       else:
-        render = add2dArrays(render, contents, AbarHt=barHt, BbarHt=len(contents) - (2 if exp else 0), add = len(contents) - barHt)
-        barHt += abs(min(barHt - len(contents), 0))
-      
-      i += ln + 2
-      
-      del isPwr, contents, ln
-    
+        contentLen = len(contents.bitmap)
+        render = addRenders(render, contents, AbarHt=barHt, BbarHt=contentLen - (2 if exp else 0))
+        barHt += max(0, contentLen) - barHt - (2 if exp else 0)
+        hang += contentLen - 4
+        del contentLen
+
+      del isPwr, contents
+
     elif eq[i] == "\\":
       #print("Found escape sequence")
       esc = None
       # Operator or escape character?
       try:
-        esc = Evaluator.Between(eq[i:], "\\", "{")
+        esc = Evaluator.Between(eq, i, "\\", "{")
       except ValueError: pass
-    
+
       # Escape character (Special Character)
       if esc == None or not esc.isalpha():
         specials = ["pi", "e", 'im', "perm", "comb"]
-        tries = [eq[i + 1:].startswith(ch) for ch in specials]
-        
-        # None of the accepted special characters were found
-        if not any(tries):
-          raise ValueError(f"Unidentified special character starting at: {eq[i:]}")
-        
-        #print(f"Found special character: {specials[tries.index(True)]}")
-        
-        # Generate render for character, then append it to the render and increment i
-        char = readGlyph(begWth + specials[tries.index(True)])
-        render = add2dArrays(render, char, AbarHt = barHt)
-        lastHeight = len(char)
-        i += len(specials[tries.index(True)])
-        
-        del specials, tries, char
-      
-      # Since it's not an escape character, it must be a function
+        for sp in specials:
+          if eq.startswith(sp, i + 1):
+            # Generate render for character, then append it to the render and increment i
+            char = readGlyph(begWth + sp)
+            render = addRenders(render, char, AbarHt = barHt)
+            lastHeight = len(char.bitmap)
+            i += len(sp)
+            del char
+
+      # Found fraction!
       elif esc == "frac":
         # Numerator & Denominator for fraction.
-        num = Evaluator.Between(eq[i:], "{", "}")
-        den = Evaluator.Between(eq[i + len(num) + 7:], "{", "}")
+        num = Evaluator.Between(eq, i, "{", "}")
+        den = Evaluator.Between(eq, i + len(num) + 7, "{", "}")
         #print(f"Setting i to: {i + len(num) + len(den) + 8}" )
         i += len(num) + len(den) + 8
-        
+
         num = genRender(num, True)
         den = genRender(den, True)
+        denLen = len(den.bitmap)
 
-        fraction = [
-          [False] * (max(len(num[0]), len(den[0])) + 1 + (max(len(num[0]), len(den[0])) % 2))
-          for _ in range(len(num) + len(den) + 2)
-        ]
-        fraction = merge2dArrays(
+        fraction = Render(max(num.width, den.width) + 2,
+                          [0 for _ in range(len(num.bitmap) + denLen + 2)])
+        fraction = mergeRenders(
           fraction,
           num,
-          (len(fraction[0]) // 2) - (len(num[0]) // 2),
-          len(den) + 2,
+          (fraction.width // 2) - (num.width // 2),
+          denLen + 2,
         )
-        fraction = merge2dArrays(
+        fraction = mergeRenders(
           fraction,
           den,
-          (len(fraction[0]) // 2) - (len(den[0]) // 2),
+          (fraction.width // 2) - (den.width // 2),
           0,
         )
-        fraction = merge2dArrays(
+        fraction = mergeRenders(
           fraction,
-          [[True] * (len(fraction[0]) - 1)],
+          Render(fraction.width - 1, [(0b1 << fraction.width - 1) - 1]),
           1,
-          len(den) + 1,
+          denLen + 1,
         )
-        render = add2dArrays(
-          render, fraction, AbarHt=barHt, BbarHt=len(den)
+        barHt = max(barHt, denLen) + hang
+        render = addRenders(
+          render, fraction, AbarHt=barHt, BbarHt=denLen
         )
-        barHt = max(barHt, len(den)) + hang
-        lastHeight = len(fraction) + hang
-        
-        del num, den, fraction
-        
+        lastHeight = len(fraction.bitmap) + hang
+
+        del num, den, fraction, denLen
+
       elif esc == "sqrt":
         # Nth root
-        n = Evaluator.Between(eq[i:], "{", "}")
-        radicand = Evaluator.Between(eq[i + len(n) + 7:], "{", "}")
+        n = Evaluator.Between(eq, i, "{", "}")
+        radicand = Evaluator.Between(eq, i + len(n) + 7, "{", "}")
         i += len(n) + len(radicand) + 8
-        
-        n = genRender(n, True) if n != "2" else [[False, False]]
+
+        n = genRender(n, True) if n != "2" else Render(2, [0])
         radicand = genRender(radicand, exp)
-        
-        stem = [[False, False]] + ([[False, True]] * ((len(radicand) + 1) // 2))
-        stem += [[True, False]] * ((len(radicand) - (len(radicand) + 1) // 2) - 4)
-        radical = [[False] * (5 + len(radicand[0]) + len(n[0])) for _ in range(max(len(radicand) + 2, len(radicand) + len(n) - 2))]
-        radical = merge2dArrays(radical, n, 0, (len(radical) - len(n)) // 2)
-        radical = merge2dArrays(radical, readGlyph("rad"), len(n[0]) - 2, 0)
-        radical = merge2dArrays(radical, stem, len(n[0]) + 1, 4)
-        radical = merge2dArrays(radical, radicand, len(n[0]) + 3, 0)
-        radical = merge2dArrays(radical, [[True] * (len(radicand[0]) + 3)], len(n[0]) + 2, len(radicand) + 1)
-        radical = merge2dArrays(radical, [[True], [True]], len(radicand[0]) + len(n[0]) + 4, len(radicand) - 1)
-        
-        render = add2dArrays(render, radical, AbarHt = barHt, BbarHt = lastFinishedBarHt)
+        radicandHeight = len(radicand.bitmap)
+        stem = Render(2, [0b00] + 
+                        ([0b01] * ((radicandHeight) // 2)) + 
+                        ([0b10] * ((radicandHeight - (radicandHeight + 1) // 2) - 3))
+                     )
+
+        radical = Render(radicand.width + n.width + 5, [0 for _ in range(2 + radicandHeight + max(0, len(n.bitmap) + 2 - radicandHeight))])
+        radical = mergeRenders(radical, n, 0, 4)
+        radical = mergeRenders(radical, readGlyph("rad"), n.width - 2, 0)
+        radical = mergeRenders(radical, stem, n.width + 1, 4)
+        radical = mergeRenders(radical, radicand, n.width + 3, 0)
+        radical = mergeRenders(radical, Render(radicand.width + 3, [(0b1 << (radicand.width + 3)) - 1]), n.width + 2, radicandHeight + 1)
+        radical = mergeRenders(radical, Render(1, [0b1, 0b1]), radicand.width + n.width + 4, radicandHeight - 1)
+
+        render = addRenders(render, radical, AbarHt = barHt, BbarHt = lastFinishedBarHt)
         barHt = max(barHt, lastFinishedBarHt) + hang
-        lastHeight = len(radical) + hang
-        
-        del n, radicand, stem, radical
-      
+        lastHeight = len(radical.bitmap) + hang
+
+        del n, radicand, stem, radical, radicandHeight
+
       del esc
 
     else:
       raise Exception(f" Unidentified character: {eq[i]}")
-    
+
     i += 1
 
-  #print("Removing overhead...")
-  count = 0
-  while True:
-    if all(not p for p in render[0]):
-      del render[0]
-      count += 1
-    else:
-      break
-  render.insert(0, [False] * len(render[0]))
-  print(f"Removed {max(0,count - 1)} empty overhead lines")
-  
-  print(f"Finished parsing {eq}")
-  lastFinishedBarHt = barHt
+  #print(f"Finished parsing {eq}")
+
+  lastFinishedBarHt = None if first else barHt
   return render
 
-
-def readGlyph(g, resParen = 0, exp = False):
-  with open("glyphs.txt", "r") as glyphs:
-    glyphs = glyphs.readlines()
-    line = glyphs[glyphDict[g]].replace("\n", "").replace(g, "", 1).replace(":", "")
-
-    width = 6 if len(line) == 0 else int(line.rpartition("x")[0])
-    height = 10 if len(line) == 0 else int(line.rpartition("x")[2])
-    
-    glyph = [[False] * width] * height
-
-    for y in range(height):
-      line = [
-        True if digit == "1" else False
-        for digit in bin(int(glyphs[glyphDict[g] + y + 1]))[2:]
-      ]
-      line = [False] * (width - len(line)) + line
-      glyph[y] = line
+def readGlyph(g, resizeBy = 0, exponent = False, absVal = False):
+  try:
+    if exponent and abs(resizeBy) > 10:
+      exponent = False
+      g = g[2]
       
-    for p in range(abs(resParen)):
-      glyph.insert(
-        4,
-        ([False, True, False] if resParen < 0 else [False, False, True]) if exp 
-        else ([False, True, False, False]
-          if resParen < 0
-          else [False, False, False, True]
-          )
-      )
+    glyph = prerendered.prerenderedGlyphs[g]
+    glyph = Render(glyph[0], glyph[1].copy() if resizeBy != 0 else glyph[1]) # See prerendered.py {Tuple: (w, bmap)}
+
+    if resizeBy != 0:
+      glyph.bitmap[4:4] = [1 if absVal else ((2 if exponent else 4) if resizeBy < 0 else 1)] * abs(resizeBy)
 
     return glyph
 
-  raise Exception(f"Glyph not found '{g}'")
+  except KeyError:
+    raise Exception(f"Glyph not found '{g}'")
+
+class Render:
+  def __init__(self, w, bmap = None, convert = False):
+    if convert:
+      newBmap = []
+      for row in bmap:
+        rowInt = 0
+        for bit in row:
+          rowInt = (rowInt << 1) | (bit & 0b1)
+        newBmap.append(rowInt)
+      
+      bmap = newBmap
+
+    self.bitmap = bmap or []
+    self.width = w
+
+printGlyphs = {
+  0b0000: "  ",
+  0b0001: " ▄",
+  0b0010: "▄ ",
+  0b0011: "▄▄",
+  0b0100: " ▀",
+  0b0101: " █",
+  0b0110: "▄▀",
+  0b0111: "▄█",
+  0b1000: "▀ ",
+  0b1001: "▀▄",
+  0b1010: "█ ",
+  0b1011: "█▄",
+  0b1100: "▀▀",
+  0b1101: "▀█",
+  0b1110: "█▀",
+  0b1111: "██"
+}  
+def testPrint(render, prettyPrint=False):
+  if not isinstance(render, Render):
+    raise TypeError(f"render ({render}) is not of type Render and cannot be testPrinted")
+
+  if prettyPrint:
+    if len(render.bitmap) % 2 != 0:
+      render.bitmap.append([0])
+    if render.width % 2 != 0:
+      render.width += 1
+    
+    for y in range(0, len(render.bitmap), 2):
+      for x in range(0, render.width, 2):
+        key = (
+          (0b1000 if (render.bitmap[y] >> (render.width - x - 1)) & 1 else 0) |
+          (0b0100 if (render.bitmap[y] >> (render.width - x - 2)) & 1 else 0) |
+          (0b0010 if (render.bitmap[y + 1] >> (render.width - x - 1)) & 1 else 0) |
+          (0b0001 if (render.bitmap[y + 1] >> (render.width - x - 2)) & 1 else 0)
+        )
+        print(printGlyphs[key], end="")
+      print()
+
+  else:
+    print("[", end="")
+    for i, row in enumerate(render.bitmap):
+      print("0b" + ('0' * (render.width - max(1, row.bit_length())) + f"{row:b}"), end="," if i < len(render.bitmap) - 1 else "")
+    print("]")
 
 
-def add2dArrays(a, b, overlap=-1, relHt=-1, AbarHt=-1, BbarHt=-1, add = 0):
-  if relHt == -1:
-    relHt = len(a)
-  if AbarHt == -1:
-    AbarHt = (len(a) // 2) - 1
-  if BbarHt == -1:
-    BbarHt = (len(b) // 2) - 1
-  diff = (AbarHt - BbarHt) if overlap == -1 else 0
-
-  #print(f"Add2dArrays ARGS: overlap: {overlap}, relHt: {relHt}, AbarHt: {AbarHt}, BbarHt: {BbarHt}, diff: {diff}")
-
-  newArray = [[False] * (len(a[0]) + len(b[0]))] * (
-    max(
-      max(len(a), len(b)) + ((len(b) - BbarHt - 1) - (len(a) - AbarHt - 1)),
-      len(a),
-      len(b),
-    ) + add
-    if overlap == -1
-    else max(len(a), relHt + len(b) - overlap)
-  )
-  newArray = merge2dArrays(newArray, a, 0, abs(diff) if diff < 0 else 0)
-  newArray = merge2dArrays(
-    newArray,
-    b,
-    len(a[0]),
-    (diff if diff > 0 else 0) if overlap == -1 else (relHt - overlap),
-  )
-
-  return newArray
-
-
-def merge2dArrays(a, b, x, y):
-  y = len(a) - y
-  for h in range(len(a)):
-    toMerge = (
-      [False] * len(a[0])
-      if not (h <= y and h > y - len(b))
-      else ([False] * x) + b[h - y] + ([False] * (len(a[0]) - len(b[0]) - x))
-    )
-    a[h] = [max(a[h][i], toMerge[i]) for i in range(len(toMerge))]
+def mergeRenders(a, b, x, y):
+  lenA = len(a.bitmap)
+  y = lenA - y
+  
+  for h in range(y + 1 - len(b.bitmap), min(lenA, y + 1)):
+    a.bitmap[h] |= b.bitmap[h - y] << a.width - x - b.width
 
   return a
 
+def addRenders(a, b, overlap=-1, relHt=-1, AbarHt=-1, BbarHt=-1):
+  heightA = len(a.bitmap)
+  heightB = len(b.bitmap)
+  if relHt == -1:
+    relHt = heightA
+  if AbarHt == -1:
+    AbarHt = (heightA // 2) - 1
+  if BbarHt == -1:
+    BbarHt = (heightB // 2) - 1
+  diff = (AbarHt - BbarHt) if overlap == -1 else 0
 
-def print2dArray(arr, bh = None):
-  if bh != None:
-    H = [[False, False]] * len(arr)
-    H[len(H) - 1 - bh] = [True, False]
-    arr = add2dArrays(H, arr)
-  
-  print(f"Dimensions: {len(arr[0])}x{len(arr)}")
-  print("--PRERENDER--")
-  for y in range(len(arr)):
-    for x in range(len(arr[0])):
-      print("██" if arr[y][x] else "  ", end="")
-    print()
-  print("--PRERENDER--")
-  
-equation = r"4 - 3 - 4\im"
-eq = equation
+  #print(f"addRenders ARGS: overlap: {overlap}, relHt: {relHt}, AbarHt: {AbarHt}, BbarHt: {BbarHt}, diff: {diff}")
 
-if "=" in eq:
-  indx = eq.index("=")
-  eq = eq[:indx] + " - ("+eq[indx + 1:]+")"
-  
-ans = str(Evaluator.Evaluate(eq, "x" in eq, True, 0.2))
-ans = (float(ans) if isinstance(ans, float) else
-      "{0:g}".format(complex(ans).real) + ("{0:+g}".format(complex(ans).imag) + r"\im" if 
-                                complex(ans).imag != 0 else "")).replace("j", r"\im")
-ans = "~" + ans[1:] if ans[0] == "-" else ans
+  newHeight = ((heightA + max((AbarHt - heightA) - (BbarHt - heightB), 0) + max(BbarHt - AbarHt, 0))
+              if overlap == -1
+              else max(heightA, relHt + heightB - overlap))
+  newArray = Render(a.width + b.width, [0 for _ in range(newHeight)])
+  newArray = mergeRenders(newArray, a, 0, (-diff if diff < 0 else 0))
+  newArray = mergeRenders(
+    newArray,
+    b,
+    a.width,
+    (diff if diff > 0 else 0) if overlap == -1 else (relHt - overlap),
+  )
 
-renderEQ = genRender(equation)
-renderANS = genRender(("x" if "x" in equation else "") + "=" + ans)
-
-print2dArray(renderEQ)
-print2dArray(renderANS)
-
-
+  del a, b
+  return newArray
---
+++ code.py
@@ -0,0 +1,76 @@
+import LiTeX as LT # type: ignore
+import Evaluator as EV # type: ignore
+import SPI
+import time
+
+print("Initializing Display...")
+SPI.initialize_display()
+time.sleep(1)
+SPI.software_reset()
+
+print("Clearing Display...")
+SPI.clear_display()
+SPI.send_command(SPI.INVR_OFF)
+
+print("Wait 1 sec...")
+time.sleep(1)
+
+eqs = [
+  r"1",
+  r"1 + ",
+  r"1 + \e",
+  r"1 + \e^{`}",
+  r"1 + \e^{2}",
+  r"1 + \e^{2\pi}",
+  r"1 + \e^{2\pi\im}",
+  r"1 + \e^{2\pi\im} -",
+  r"1 + \e^{2\pi\im} - 3",
+  r"1 + \e^{2\pi\im} - 3\sqrt{2}{`}",
+  r"1 + \e^{2\pi\im} - 3\sqrt{2}{\frac{`}{`}}",
+  r"1 + \e^{2\pi\im} - 3\sqrt{2}{\frac{sin(`)}{`}}",
+  r"1 + \e^{2\pi\im} - 3\sqrt{2}{\frac{sin(4)}{`}}",
+  r"1 + \e^{2\pi\im} - 3\sqrt{2}{\frac{sin(4)}{3}}",
+]
+
+renderEQ = None
+print()
+for eqNum in range(len(eqs)):
+  start = time.monotonic_ns()
+  renderEQ = LT.genRender(eqs[eqNum])
+  print(f"rendered in {(time.monotonic_ns() - start) / 1_000_000}ms")
+
+  print("Sending Data...\n")
+  SPI.send_render(renderEQ, 0)
+
+  #LT.testPrint(renderEQ, True)
+
+equation = eqs[-1]
+eq = equation
+
+if "=" in eq:
+  indx = eq.index("=")
+  eq = eq[:indx] + " - ("+eq[indx + 1:]+")"
+
+ans = EV.Evaluate(eq, solve="x" in eq, replace=True, guess=2, shouldGuessImag=False)
+ans = (float(ans) if isinstance(ans, float) else
+      "{0:10f}".format(ans.real).rstrip("0").rstrip(".") + ("{0:+10f}".format(ans.imag).rstrip("0").rstrip(".") + r"\im" if
+            ans.imag != 0 else "")).replace("j", r"\im")
+ans = "~" + ans[1:] if ans[0] == "-" else ans
+ans = ans.replace("j", "\im")
+renderANS = LT.genRender(("x" if "x" in equation else "") + "=" + ans)
+del eq, ans, equation
+
+#LT.print2dArray(renderEQ)
+#LT.print2dArray(renderANS)
+
+print("Sending Data...")
+SPI.send_render(renderEQ, 0)
+SPI.send_render(renderANS, 64 - len(renderANS.bitmap))
+
+print("Press enter to close...")
+_ = input()
+
+print("Clearing, Software reset, and closing SPI...")
+SPI.clear_display()
+SPI.software_reset()
+SPI.spi.unlock()
