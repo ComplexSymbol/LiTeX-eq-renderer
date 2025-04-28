bd99533fd541247c37033d30967fd0e762d316e2
ComplexSymbol <141301057+ComplexSymbol@users.noreply.github.com>
2025-04-27 21:09:55 -0700

    Prerendered glyphs
    c++ isn't that hard actually (:
    
    ... famous last words

--- cpp/LiTeX.cpp
+++ cpp/LiTeX.cpp
@@ -4,12 +4,13 @@
 #include "renderFormat.cpp"
 
 using namespace std;
-void Testing() {
-    Render a = Render(vector<unsigned long long>({0, 0b10100100, 0b01010010, 0x1f, 0, 0x1f}), 8);
-    Render b = Render(vector<unsigned long long>({0x1f, 0, 0x1f, 0, 0xf1, 0}), 8);
-    a.Print(true);
-    b.Print(true);
-    
-    a = appendRenders(a, b, 0, 0, 2, 3);
-    a.Print(true);
-};
+Render GenerateRender(string eq, bool exp = false) {
+    for (char c : eq) {
+
+    }
+}
+
+void Test() {
+    Render r = Render(vector<ull>({ 0, 65, 129, 511, 1, 1 }), 10);
+    r.Print(true);
+}
